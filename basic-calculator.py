d379b55b8ffdf2037888b64061a590aba85b77d9
“YOUR <shnnagai@gmail.com>
2018-11-26 23:23:54 +0900

    postfix done

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,15 +1,60 @@
 class Solution:
     signs = ['-', '+', '(']
     mathSigns = ['-', '+']
-
+    allSigns = ['-', '+', '(',')']
     def calculate(self, s):
         """
         :type s: str
         :rtype: int
         """
+        # take care of the edge cases (1) 0 
+
+        s = s.replace(" ","")
+        self.buildArr(s)
         stack = self.getPostfix(s)
-       # print(stack)
+        result = self.postFix(stack)
+        print(result)
+        return result
     
+    def postFix(self, stack):
+        s = []
+        while len(stack) != 0:
+            pop = stack.pop(0)
+            if pop in self.mathSigns:
+                top = s.pop()
+                buttom = s.pop()
+                val = self.calc(top, buttom, pop)
+                s.append(val)
+            else:
+                s.append(pop)
+        return int(s[0])
+
+    def buildArr(self, s):
+        stack = []
+        num = ""
+        for i in range(len(s)):
+            if s[i] in self.allSigns and num == "":
+                stack.append(s[i])
+            elif s[i] in self.allSigns and num != "":
+                stack.append(num)
+                stack.append(s[i])
+                num = ""
+            else:
+                num = num + s[i]
+        
+        if num != "":
+            stack.append(num)
+
+        print(stack)
+        
+    def calc(self, top, buttom, operator):
+        val = 0
+        if operator == '+':
+            val = int(buttom) + int(top)
+        else:
+            val = int(buttom) - int(top)
+        return val
+
     def getPostfix(self, s):
         output = []
         operator = []
@@ -36,14 +81,14 @@ class Solution:
                 pop = operator.pop()
                 output.append(pop)
 
-        print("operator")
-        print(operator)
-        print("stack")
-        print(output)
+        # print("operator")
+        # print(operator)
+        # print("stack")
+        # print(output)
         
         return output
         
 
 s = Solution()
-equation = "(1+(4+5+2)-3)+(6+8)" # 3.5
+equation = "2147483647+10+(5+4)" # 3.5
 s.calculate(equation)
