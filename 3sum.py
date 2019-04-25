ebc22c11c55cc3026538e5bf0a29b9f2c8fc1043
Shin Nagai <shnnagai@gmail.com>
2019-04-25 09:47:56 +0900

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -8,35 +8,22 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         sol = []
-        self.threeSumhelper(nums, sol)
-    
-    def threeSumhelper(self, nums, sol = []):
-
-        s = sum(sol)
-        print(nums)
-        if len(sol) == 3 and s == 0:
-            print(sol)
-        elif len(sol) >= 3:
+        ans = []
+        return self.threeSumhelper(nums, sol, ans)
+       
+    def threeSumhelper(self, numbers, partial = [], ans = []):
+        if len(partial) == 3 and sum(partial) == 0:
+            return ans.append(partial)
+        elif len(partial) >= 3:
             return
-
-        for i in range(len(nums)):
-            n = nums[i]
-            remaining = nums[i+1:]
-            return self.threeSumhelper(remaining, sol + [n])
-
-    def subset_sum(self, numbers, partial=[]):
-        s = sum(partial)
-        print(partial)
-        # check if the partial sum is equals to target
-        if len(partial) == 3 and s == 0:
-            print(partial)
-        if s >= 3:
-            return  # if we reach the number why bother to continue
 
         for i in range(len(numbers)):
             n = numbers[i]
             remaining = numbers[i+1:]
-            self.subset_sum(remaining, partial + [n])
+            ans = self.threeSumhelper(remaining, partial + [n])
+        return ans
+    
+
 
 nums = [-1, 0, 1, 2, -1, -4]
 s = Solution()
