87b474ee7479d1d3b60133dc43a3c6f82cf59bf8
“YOUR <shnnagai@gmail.com>
2019-04-30 10:19:17 +0900

    permutation.py

---
+++ algo/permutation.py
@@ -0,0 +1,19 @@
+def getPermutations(array):
+    # Write your code here.
+    permutations = []
+    getPermutationsHelper(array, [], permutations)
+    print(permutations)
+    return permutations
+
+
+def getPermutationsHelper(array, currentPermutation, permutations):
+    if not len(array) and len(currentPermutation):
+        permutations.append(currentPermutation)
+    else:
+        for i in range(len(array)):
+            newArray = array[:i] + array[i+1:]
+            newPermutation = currentPermutation + [array[i]
+            getPermutationsHelper(newArray, newPermutation, permutations)
+
+a = [1,2,3,4]
+getPermutations(a)
--- flightItinerary_Backtracking.py
+++ flightItinerary_Backtracking.py
@@ -16,24 +16,45 @@ def get_itinerary(flights, current):
     return None
 
 
-# flights = [["HNL", "AKL"], ["YUL", "ORD"], ["ORD", "SFO"], ["SFO", "HNL"]]
-# current = ["YUL"]
-# print(get_itinerary(flights, current))
+"""
+ diff version
+"""
+# def flightIntenary(flights)
+	
+# 	res = []
+# 	origin = "YUL"
+# 	return flightIntenaryHelper(flights, current,res)
+
+# def flightIntenaryHelper(flights, current, res):
+
+# 	if not len(flights):
+# 		return res
+
+# 	for i, (origin, destination) in enumerate(flights):
+# 		if current == origin:
+# 			res.append(origin)
+# 			newFlight = flights[:i] + flights[i+1:]
+# 			flightIntenaryHelper(newFlights, destination, res)
+# 	return None
+
+flights = [["HNL", "AKL"], ["YUL", "ORD"], ["ORD", "SFO"], ["SFO", "HNL"]]
+current = ["YUL"]
+print(get_itinerary(flights, current))
 
 
-def subset_sum(numbers, target, partial=[]):
-    s = sum(partial)
-    print(partial)
-    # check if the partial sum is equals to target
-    if s == target: 
-        print "sum(%s)=%s" % (partial, target)
-    if s >= target:
-        return  # if we reach the number why bother to continue
+# def subset_sum(numbers, target, partial=[]):
+#     s = sum(partial)
+#     print(partial)
+#     # check if the partial sum is equals to target
+#     if s == target: 
+#         print "sum(%s)=%s" % (partial, target)
+#     if s >= target:
+#         return  # if we reach the number why bother to continue
 
-    for i in range(len(numbers)):
-        n = numbers[i]
-        remaining = numbers[i+1:]
-        subset_sum(remaining, target, partial + [n]) 
+#     for i in range(len(numbers)):
+#         n = numbers[i]
+#         remaining = numbers[i+1:]
+#         subset_sum(remaining, target, partial + [n]) 
         
-subset_sum([3,9,8,4,5,7,10],15)
+# subset_sum([3,9,8,4,5,7,10],15)
 
