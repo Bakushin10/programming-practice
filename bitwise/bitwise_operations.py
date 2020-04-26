c9a707589983c7fe622b3dc43db3746d4954465d
shin nagai <shnnagai@gmail.com>
2020-04-26 22:41:28 +0900

    fix : update bitwise.py comment

--- bitwise/bitwise_operations.py
+++ bitwise/bitwise_operations.py
@@ -1,4 +1,5 @@
 def is_chosen_color(chosen_colors, color):
+    # 'chosen_colors & color' is binary AND
 	return color == chosen_colors & color
 
 
@@ -10,7 +11,7 @@ pink = green << 1
 white = pink << 1
 black = white << 1
 
-
+# binary OR to include all chosen colors
 chosen_colors = red | yellow | white | black
 print(f"white : {is_chosen_color(chosen_colors, white)}")
 print(f"yellow : {is_chosen_color(chosen_colors, yellow)}")
